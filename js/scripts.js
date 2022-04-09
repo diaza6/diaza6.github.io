7bca8c79006ba9a80270ff503790c6ace5d4968b
diaza6 <diaza6@uw.edu>
2022-04-08 19:14:03 -0700

    Add website and update resume

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,142 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+"use strict";
+
+ (function() {
+
+   const pc = [
+       "Rocket League", "Grand Theft Auto V", "Star Wars Battlefront 2",
+       "Halo The Master Chief Collection", "Fortnite", "Valheim", "Lego Star Wars The Complete Saga",
+       "Star Wars the Force Unleashed", "Deus Ex", "Roblox", "Minecraft", "Metro Exodus", "Apex Legends",
+       "Titanfall 2", "Star Wars Jedi Fallen Order", "Fall Guys Ultimate Knockout", "Doom",
+       "The Elder Scrolls V Skyrim", "Slime Rancher", "No Man's Sky", "Little Nightmares",
+       "Need for Speed Heat", "The Witcher", "Left 4 Dead 2", "Among Us", "Dying Light",
+       "Batman Arkham Asylum", "Outer Wilds", "Red Dead Redemption 2", "Detriot Become Human",
+       "The Outer Wilds", "Far Cry 3", "Assassin's Creed Odyssey", "Watch_Dogs",
+       "Dark Souls", "Sonic Mania", "The Forest", "Lego Marvel's Avengers", "Rust",
+       "Subnautica", "Horizon Zero Dawn", "Forza Horizon 4", "Marvel's Avengers",
+       "Back 4 Blood", "Sea of Thieves", "Forza Horizon 5", "Cyberpunk 2077",
+       "Guardians of the Galaxy"
+    ]
+
+    const playstation = [
+        "Marvel's Spider-Man", "Marvel's Spider-Man: Miles Morales", "The Last of Us",
+        "Resident Evil 7 Biohazard", "Fallout 4", "Until Dawn", "Call of Duty: Black Ops 3",
+        "inFAMOUS Second Son", "Ratchet & Clank"
+    ]
+
+    const xbox = [
+        "Halo Infinite", "Grand Theft Auto: San Andreas", "Exo One", "Gears of War",
+        "SSX", "Rainbow Six Siege", "Mass Effect Legendary Edition", "Rainbow Six Extraction",
+        "Gang Beasts", "Grand Theft Auto IV", "Midnight Club Los Angeles"
+    ]
+
+    const nintendo = [
+        "Super Smash Bros. Ultimate", "Super Mario Bros. U Deluxe", "Mario Cart 8 Deluxe",
+        "Super Mario 3D World + Bowser's Fury", "The Legend of Zelda: Breath of the Wild",
+        "Super Mario Party"
+    ]
+
+    const BASE_URL = "https://api.rawg.io/api/games?search="
+
+    const API_KEY = "&key=ed51b3b6df6e4ebea25c5dfce00a6552"
+
+   window.addEventListener("load", init);
+
+   function init() {
+        id("get-game").addEventListener("click", getGameNames)
+   }
+
+   function getGameNames() {
+        let pcGame = pc[Math.floor(pc.length * Math.random())];
+        let ps5Game = playstation[Math.floor(playstation.length * Math.random())];
+        let xseriesGame= xbox[Math.floor(xbox.length * Math.random())];
+        let switchGame = nintendo[Math.floor(nintendo.length * Math.random())];
+        fetchInfo(pcGame, "pc");
+        fetchInfo(ps5Game, "playstation");
+        fetchInfo(xseriesGame, "xbox");
+        fetchInfo(switchGame, "switch");
+   }
+
+   function fetchInfo(name, platform) {
+    let url = BASE_URL + name + API_KEY;
+    fetch(url)
+      .then(checkStatus)
+      .then(resp => resp.json())
+      .then(result => {
+        createCards(result, platform);
+      })
+      .catch(console.error);
+  }
+
+  async function checkStatus(res) {
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return res;
+  }
+
+  function createCards(results, platform) {
+    createTitles(platform);
+    let info = results.results[0];
+    let name = info.name;
+    let image = info.background_image
+    let newDiv = gen("div");
+    newDiv.classList.add("col-md-4")
+    newDiv.id ="platform-card"
+    let newImage = gen("img");
+    newImage.src = image;
+    newImage.alt = name;
+    newImage.classList.add("card-img-top", "img-thumbnail", "img-fluid");
+    let newDiv2 = gen("div");
+    newDiv2.classList.add("card-body");
+    let h3 = gen("h3");
+    h3.classList.add("card-title");
+    h3.textContent = name;
+    newDiv2.appendChild(newImage);
+    newDiv2.appendChild(h3);
+    newDiv.appendChild(newDiv2);
+    id(platform).appendChild(newDiv);
+  }
+
+  function createTitles(platform) {
+    id(platform).innerHTML = "";
+    let h2 = gen("h2");
+    h2.classList.add("p-1");
+    let img = gen("img");
+    img.classList.add("p-1");
+    img.width = "50";
+    img.height = "50";
+    let div = gen("div");
+    div.classList.add("w-100");
+    if (platform == "pc") {
+        h2.textContent = "PC Game";
+        img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/2048px-Steam_icon_logo.svg.png";
+        img.alt = "Steam logo";
+    } else if (platform == "playstation") {
+        h2.textContent = "Playstation Game";
+        img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/PlayStation_logo.svg/2560px-PlayStation_logo.svg.png";
+        img.alt = "Playstation logo";
+    } else if (platform == "xbox") {
+        h2.textContent = "Xbox Game";
+        img.src = "https://compass-ssl.xbox.com/assets/56/72/5672b413-1b6a-4307-b1df-e4ff5311ee8d.svg?n=03958693_App-Icon-0_120x120.svg";
+        img.alt = "Xbox logo";
+    } else if (platform == "switch") {
+        h2.textContent = "Switch Game";
+        img.src = "https://seeklogo.com/images/N/nintendo-switch-logo-38D4F5C7E7-seeklogo.com.png";
+        img.alt = "Switch logo";
+    }
+    id(platform).appendChild(h2);
+    id(platform).appendChild(img);
+    id(platform).appendChild(div);
+  }
+
+   function id(idName) {
+     return document.getElementById(idName);
+   }
+
+   function gen(tagName) {
+     return document.createElement(tagName);
+   }
+ })();
--- resume.md
+++ resume.md
@@ -1,15 +1,23 @@
 # Angel Diaz (he/him)
+
 **Email:** diaza6@uw.edu
+
 [LinkedIn](https://www.linkedin.com/in/angel-diaz-anaya/)
+
 ![Hide the Pain Harold](https://i.imgflip.com/2i8ld7.jpg)
 
 > Incoming Tech Arch Delivery Analyst at Accenture
 
 ## Education
+
 [University of Washington](www.washington.edu), Seattle, WA
+
 __B.A. Geography: Data Science__
+
 __Minor: Informatics__
+
 __GPA:__ **3.5**
+
 __Dean's List:__ **9 quarters**
 
 ## Honors
@@ -20,6 +28,7 @@ Scholarships:
 - CAMP Scholar
 
 ## Skills
+
 - Bilingual (English, Spanish)
 - Microsoft Office (Word, Excel, PowerPoint)
 - HTML5, CSS, JavaScript
@@ -30,26 +39,32 @@ Scholarships:
 - Customer Relations and Service
 
 ## Leadership
+
 ### [SHPE](uwshpe.com) | Webmaster & Director of Advertising
 - Created media to market events, documented events for future advertisements, and managed all social media accounts
 - Managed student resources, such as scholarships, and published online
 - Created and deployed web content relevant to organizational news and events
+
 ### [Omega Delta Phi Fraternity](https://odphiuw.wixsite.com/odphi-uw) | Vice President & Treasurer
 - Oversee projects and committees to further the organization’s goals
 - Reported account summaries by monitoring deposits and expense transactions
 - Projected and recommended a quarterly budget to ensure the organization’s spending stays within margins
 
-## WORK EXPERIENCE
-### CONSULTING SUMMER ANALYST | ACCENTURE
+## Work Experience
+
+### Consulting Summer Analyst | Accenture
 - Managed API automation testing for client web application
 - Initiated work on test plan & strategy
-### CASHIER ASSISTANT | COSTCO
+
+### Cashier Assistant | Costco
 - Assisted customers needs by paying attention to detail, advising on sale items, and supporting ad-hoc request
 - Collaborated with management to coordinate effectively on daily tasks and needs of support at store
 - Managed all customer relations in accordance to company policy and escalate any risk and concerns to management
-### CREW MEMBER | MCDONALD’S
+
+### Crew Member | McDonald’s
 - Partnered with other Crew and Managers to meet target goals and monitor performance during shift
 - Delivered prompt service and adhered to strict FIFO rules in food preparation
-### OVERNIGHT MAINTENANCE | WALMART
+
+### Overnight Maintenance | Walmart
 - Communicated with other departments to ensure colleagues met work duty expectations in a timely fashion
 - Performed sanitation procedures to maintain environment and ensure all tasks performed met necessary corporate standard
